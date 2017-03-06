2b46d1894e259b1ee853c827df0035f57bec4b2d
james341 <taine049@gmail.com>
2017-03-07 02:53:07 +1100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -55,7 +55,8 @@ Multiple images will automatically shuffle between them
 var l_bgImages = [
 	"touhou01.jpg",
 	"touhou02.jpg",
-	"touhou03.jpg"
+	"touhou03.jpg",
+	"touhou04.jpg"
 ];
 
 /*Random background images order?*/
