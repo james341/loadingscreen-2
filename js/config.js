c403fe4e4dd4ebf2e0352e53e720a3f452584d0b
james341 <taine049@gmail.com>
2018-11-29 02:02:05 +1100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -65,7 +65,24 @@ var l_bgImages = [
         "image10.jpg",
         "image11.jpg",
         "image12.jpg",
-
+        "image13.jpg",
+        "image14.jpg",
+        "image15.jpg",
+        "image16.jpg",
+        "image17.jpg",
+        "image18.jpg",
+        "image19.jpg",
+        "image20.jpg",
+        "image21.jpg",
+        "image22.jpg",
+        "image23.jpg",
+        "image24.jpg",
+        "image25.jpg",
+        "image26.jpg",
+        "image27.jpg",
+        "image28.jpg",
+        "image29.jpg",
+        "image30.jpg",
 ];
 
 /*Random background images order?*/
