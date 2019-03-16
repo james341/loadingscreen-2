46df5eefcc96932e4434fa6b3bbe5e659fe2edc7
james341 <taine049@gmail.com>
2019-03-16 21:40:23 +1100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -116,15 +116,15 @@ var l_musicDisplay = true;
 Place how much .ogg songs you want
 (Put the files in the songs folder)*/
 var l_musicPlaylist = [
-	{youtube: "Sq2FkmVnFcc", name: "atlas - Installation Music"},
+	{youtube: "9bU-2Ywl0F4", name: "atlas - Installation Music"},
 	{youtube: "ZiUBEr3ZZx4", name: "atlas - Installation Music"},
         {youtube: "PimD6LtlMtM", name: "atlas - Installation Music"},
         {youtube: "XK1n-Yv6N4E", name: "atlas - Installation Music"},
-        {youtube: "", name: "atlas - Installation Music"},
-        {youtube: "", name: "atlas - Installation Music"},
-        {youtube: "", name: "atlas - Installation Music"},
-        {youtube: "", name: "atlas - Installation Music"},
-        {youtube: "", name: "atlas - Installation Music"},
+        {youtube: "sUTdaqV8SLs", name: "atlas - Installation Music"},
+        {youtube: "VTt0-y3tGU0", name: "atlas - Installation Music"},
+        {youtube: "AO47K4LkB2c", name: "atlas - Installation Music"},
+        {youtube: "9bU-2Ywl0F4", name: "atlas - Installation Music"},
+        {youtube: "3af_y4tWIy8", name: "atlas - Installation Music"},
 
 ];
 
